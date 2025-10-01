0511e5b1bc2a673f5e312fa031e75304296bd46d
Garvit1809 <garvitvarshne18@gmail.com>
2025-10-01 12:34:27 +0530

    minor change

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["res.cloudinary.com"],
+    // list every remote host you load images from
+    remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
   },
 };
 
